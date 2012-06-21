372d82ab462dff9ee27123d1162352ac910c613d
beyamor <beyamor@gmail.com>
2012-06-21 12:41:19 -0700

    Created a Layer class to wrap up that swag

--- src/gfx/parallax.py
+++ src/gfx/parallax.py
@@ -1,49 +1,59 @@
 import math
 
 class Parallaxor:
+
+	class Layer:
+		def __init__(self, image, view, shift_factor):
+			self.image = image
+			self.step_x = view.from_pixels(image.get_width())
+			self.step_y = view.from_pixels(image.get_height())
+			self.shift_factor = shift_factor
+		
 	view = None
 	backgrounds = []
 
 	def __init__(self, view):
 		self.view = view
 
+	def next_background_factor(self):
+		return 1 - 0.9 ** (len(self.backgrounds) + 1)
+
 	def push_background(self, image):
-		self.backgrounds.append(image)
+		self.backgrounds.append(
+				Parallaxor.Layer(
+					image,
+					self.view,
+					self.next_background_factor()))
 
 	def draw(self):
 
-		for layer in range(len(self.backgrounds)):
-			image = self.backgrounds[layer]
-
-			shift_factor = 1 - 0.9 ** (layer+1)
+		for layer in self.backgrounds:
+			shift_factor = layer.shift_factor
 			ox = self.view.origin[0]
 			oy = self.view.origin[1]
 			shifted_ox = ox * shift_factor
 			shifted_oy = oy * shift_factor
 
-			step_x = self.view.from_pixels(image.get_width())
-			step_y = self.view.from_pixels(image.get_height())
-
 			min_x = shifted_ox
 			while min_x > ox:
-				min_x -= step_x
-			while min_x + step_x < ox:
-				min_x += step_x
+				min_x -= layer.step_x
+			while min_x + layer.step_x < ox:
+				min_x += layer.step_x
 			min_y = shifted_oy
 			while min_y > oy:
-				min_y -= step_y
-			while min_y + step_y < oy:
-				min_y += step_y
+				min_y -= layer.step_y
+			while min_y + layer.step_y < oy:
+				min_y += layer.step_y
 
-			max_x = min_x + self.view.width + step_x * 2
-			max_y = min_y + self.view.height + step_y * 2
+			max_x = min_x + self.view.width + layer.step_x * 2
+			max_y = min_y + self.view.height + layer.step_y * 2
 
 			x = min_x
 			while x <= max_x:
 				y = min_y
 				while y <= max_y:
 					self.view.draw_image(
-							image,
+							layer.image,
 							(x,y))
-					y += step_y
-				x += step_x
+					y += layer.step_y
+				x += layer.step_x
