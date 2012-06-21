9b371fc717371addbcf6aaf32d9e3c30827f6ebe
beyamor <beyamor@gmail.com>
2012-06-21 10:51:39 -0700

    Parallax scrolling w/ chinced math

--- src/gfx/parallax.py
+++ src/gfx/parallax.py
@@ -1,3 +1,5 @@
+import math
+
 class Parallaxor:
 	view = None
 	backgrounds = []
@@ -12,14 +14,36 @@ class Parallaxor:
 
 		for layer in range(len(self.backgrounds)):
 			image = self.backgrounds[layer]
-			origin_x = self.view.origin[0] * (1 - 0.8 ** (layer+1))
-			origin_y = self.view.origin[1] * (1 - 0.8 ** (layer+1))
-			step_h = self.view.from_pixels(image.get_width())
-			step_v = self.view.from_pixels(image.get_height())
 
-			for i in range(10):
-				for j in range(10):
+			shift_factor = 1 - 0.8 ** (layer+1)
+			ox = self.view.origin[0]
+			oy = self.view.origin[1]
+			shifted_ox = ox * shift_factor
+			shifted_oy = oy * shift_factor
+
+			step_x = self.view.from_pixels(image.get_width())
+			step_y = self.view.from_pixels(image.get_height())
+
+			min_x = shifted_ox
+			while min_x > ox:
+				min_x -= step_x
+			while min_x + step_x < ox:
+				min_x += step_x
+			min_y = shifted_oy
+			while min_y > oy:
+				min_y -= step_y
+			while min_y + step_y < oy:
+				min_y += step_y
+
+			max_x = min_x + self.view.width + step_x * 2
+			max_y = min_y + self.view.height + step_y * 2
+
+			x = min_x
+			while x <= max_x:
+				y = min_y
+				while y <= max_y:
 					self.view.draw_image(
 							image,
-							(origin_x+step_h*i,
-								origin_y+step_v*j))
+							(x,y))
+					y += step_y
+				x += step_x
