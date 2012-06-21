f30e9cab3aade7fada6bf94372a69aaf4d1cec45
beyamor <beyamor@gmail.com>
2012-06-20 22:07:56 -0700

    Fixed some view stuff

--- src/gfx/view.py
+++ src/gfx/view.py
@@ -4,16 +4,30 @@ class View:
 	screen = None
 	scale = 1
 	origin = (0,0)
+	width = 0
+	height = 0
 
 	def __init__(self, screen, scale, origin):
 		self.screen = screen
 		self.scale = scale
+		self.width = self.from_pixels(screen.get_width())
+		self.height = self.from_pixels(screen.get_height())
 		self.origin = origin
+
+	def from_pixels(self, number):
+		return number/self.scale
+
+	def to_pixels(self, number):
+		return number*self.scale
+
+	def center(self, position):
+		self.origin = (position[0]-self.width/2.,
+				position[1]-self.height/2.)
 
 	def draw_image(self, image, pos, angle=0, color=None):
 
-		x = (self.origin[0] + pos[0]) * self.scale
-		y = (self.origin[1] + pos[1]) * self.scale
+		x = (pos[0] - self.origin[0]) * self.scale
+		y = (pos[1] - self.origin[1]) * self.scale
 
 		draw.image(
 				self.screen,
